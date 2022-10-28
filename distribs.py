fa2ca26133fbdecac65cb2ff90204c75893e0b41
jdchang1 <jchang1730@gmail.com>
2022-10-28 15:33:54 -0400

    clamping for tanh transform

--- distribs.py
+++ distribs.py
@@ -25,14 +25,14 @@ class TanhTransform(pyd.transforms.Transform):
         return x.tanh()
 
     def _inverse(self, y):
-        # We do not clamp to the boundary here as it may degrade the performance of certain algorithms.
         # one should use `cache_size=1` instead
-        return self.atanh(y)
+        eps = torch.finfo(y.dtype).eps
+        return self.atanh(y.clamp(min=-1 + eps, max=1 - eps))
 
     def log_abs_det_jacobian(self, x, y):
         # We use a formula that is more numerically stable, see details in the following link
         # https://github.com/tensorflow/probability/commit/ef6bb176e0ebd1cf6e25c6b5cecdd2428c22963f#diff-e120f70e92e6741bca649f04fcd907b7
-        return 2. * (math.log(2.) - x - F.softplus(-2. * x))
+        return 2.0 * (math.log(2.0) - x - F.softplus(-2.0 * x))
 
 
 class SquashedNormal(pyd.transformed_distribution.TransformedDistribution):
@@ -66,9 +66,7 @@ class TruncatedNormal(pyd.Normal):
 
     def sample(self, clip=None, sample_shape=torch.Size()):
         shape = self._extended_shape(sample_shape)
-        eps = _standard_normal(shape,
-                               dtype=self.loc.dtype,
-                               device=self.loc.device)
+        eps = _standard_normal(shape, dtype=self.loc.dtype, device=self.loc.device)
         eps *= self.scale
         if clip is not None:
             eps = torch.clamp(eps, -clip, clip)
